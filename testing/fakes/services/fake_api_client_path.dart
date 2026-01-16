09d75b7cf4073f5a8a46f95967128c0309de0acb
BouncyBox <camilafontes56@gmail.com>
2026-01-16 00:11:38 -0300

    Atualiza fake api client

--- testing/fakes/services/fake_api_client_path.dart
+++ testing/fakes/services/fake_api_client_path.dart
@@ -1,25 +1,11 @@
-
-import 'package:ajudaki_mobile_clean_architecture/data/services/api/paths/api_client_path.dart';
-import 'package:ajudaki_mobile_clean_architecture/utils/web/web_response.dart';
-
-class FakeApiClientPath<T> extends ApiClientPath<T> {
+class FakeApiClientPath<T> {
     FakeApiClientPath(
-    super.resource,
-    super.fromJson,
+    Map<String, T> map
   );
 
-  Future<WebResponse<Map<String, T>>> fakeGetAll({
-    required  String jsonString,
-    Map<String, String>? queryParameters,
-  }) async {
-    return WebResponse(
-      200,
-      bodyString: jsonString,
-      fromJson: (json) => json.map(
-      (key, value) => MapEntry(
-        key,
-        fromJson(value as Map<String, T>),
-      )
-    ));
+  Map<String, T> getAll({
+    required Map<String, T> map
+  }) {
+    return map;
   }
 }
