6b6bcd66cbe08c300c29bc905c8ff3665024e439
BouncyBox <camilafontes56@gmail.com>
2026-01-19 11:43:32 -0300

    Atualiza web result status code

--- lib/utils/web/web_result_status_code.dart
+++ lib/utils/web/web_result_status_code.dart
@@ -6,18 +6,19 @@ class WebResultStatusCode {
   final int code;
 
   WebResultStatusType getStatusType(final int code) {
-    if (code >= 100 && code < 200) {
-      return WebResultStatusType.informational;
-    } else if (code >= 200 && code < 300) {
-      return WebResultStatusType.success;
-    } else if (code >= 300 && code < 400) {
-      return WebResultStatusType.redirection;
-    } else if (code >= 400 && code < 500) {
-      return WebResultStatusType.clientError;
-    } else if (code >= 500 && code < 600) {
-      return WebResultStatusType.serverError;
-    } else {
-      return WebResultStatusType.unknown;
+    switch (code ~/ 100) {
+      case 1:
+        return WebResultStatusType.informational;
+      case 2:
+        return WebResultStatusType.success;
+      case 3:
+        return WebResultStatusType.redirection;
+      case 4:
+        return WebResultStatusType.clientError;
+      case 5:
+        return WebResultStatusType.serverError;
+      default:
+        return WebResultStatusType.unknown;
     }
   }
 }
